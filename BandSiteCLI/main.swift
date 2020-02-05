ec64f57c66022bf4109bfcaf1e1fc1e0718489d0
Bill Donner <billdonner@gmail.com>
2020-02-04 21:51:10 -0500

    Initial Commit

---
+++ BandSiteCLI/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BandSiteCLI
+//
+//  Created by william donner on 2/4/20.
+//  Copyright © 2020 midnightrambler. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
